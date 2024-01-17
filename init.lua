68d35276128a9d3335db3b4dc4c0c3989f40d586
LuncyBloont <xhymaker@126.com>
2024-01-18 03:14:58 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,90 +1,18 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
--- SAFE LINE
-
-vim.opt.number = true
-vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.ruler = true
-vim.opt.wrap = false
-vim.opt.sidescroll = 1
-vim.opt.scroll = 1
-vim.opt.sidescrolloff = 30
-vim.opt.scrolloff = 12
+vim.opt.number = true
+vim.opt.relativenumber = false
 vim.opt.colorcolumn = "120"
 vim.opt.textwidth = 300
-
-vim.cmd("filetype plugin on")
-vim.cmd("filetype indent on")
-vim.cmd("syntax on")
-vim.cmd("colorscheme lunaperche")
-
-vim.opt.cindent = true
-vim.opt.autoindent = true
-
-if vim.g.neovide then
-    -- TODO
-end
+vim.opt.sidescrolloff = 30
+vim.opt.scrolloff = 12
+vim.opt.sidescroll = 1
+vim.opt.scroll = 1
+vim.opt.wrap = false
 
 vim.opt.guifont = "UbuntuMono Nerd Font:h12"
-vim.opt.linespace = 5
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-})
-
-require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = {
-        "lua_ls",
-        "tsserver",
-        "clangd",
-        "ltex",
-        "cmake",
-        "pylsp",
-        "rust_analyzer"
-    }
-}
-
---- LSP:
-require("lspconfig").lua_ls.setup {
-    settings = {
-        Lua = {
-            runtime = {
-                version = "LuaJIT"
-            },
-            diagnostics = {
-                globals = {
-                    "vim"
-                }
-            }
-        }
-    }
-}
-require("lspconfig").ltex.setup {}
-require("lspconfig").tsserver.setup {}
-require("lspconfig").clangd.setup {}
-require("lspconfig").cmake.setup {}
-require("lspconfig").pylsp.setup {}
-require("lspconfig").rust_analyzer.setup {}
-
--- test ##############################################################################################################################
---            Long Line Test
--- test ###########################################################################################################
-
